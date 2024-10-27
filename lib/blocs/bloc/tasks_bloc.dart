3f93ff9c607669a7750e030cbb59c28e8e3b4a54
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-10-27 12:27:53 +0630

    change class

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -1,13 +1,30 @@
+import 'dart:async';
+
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
+import 'package:bloc_todo/models/task.dart';
 import 'package:equatable/equatable.dart';
 
 part 'tasks_event.dart';
 part 'tasks_state.dart';
 
 class TasksBloc extends Bloc<TasksEvent, TasksState> {
-  TasksBloc() : super(TasksInitial()) {
-    on<TasksEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  TasksBloc() : super(const TasksState()) {
+    on<AddTask>(_onAddTask);
+    on<UpdateTask>(_onUpdateTask);
+    on<DeleteTask>(_onDeleteTask);
+  }
+
+  void _onAddTask(AddTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState (
+      allTasks: List.from(state.allTasks)..add(event.task),
+      ));
+  }
+
+  FutureOr<void> _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
+  }
+
+  FutureOr<void> _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {
   }
 }
--- lib/blocs/bloc/tasks_state.dart
+++ lib/blocs/bloc/tasks_state.dart
@@ -1,10 +1,14 @@
 part of 'tasks_bloc.dart';
 
-sealed class TasksState extends Equatable {
-  const TasksState();
+class TasksState extends Equatable {
+
+  final List<Task> allTasks;
+  const TasksState({
+    this.allTasks = const <Task>[],
+  });
   
   @override
-  List<Object> get props => [];
+  List<Object> get props => [allTasks];
 }
 
-final class TasksInitial extends TasksState {}
+
