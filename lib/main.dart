79f1bdcac9e6a05608112ae3d61915fdcf1272e8
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-10-27 13:22:43 +0630

    add BlocOverrides and change BlocBuilder in tasks_screen and create tasks_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:bloc_todo/blocs/bloc_exports.dart';
+import 'package:bloc_todo/models/task.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/tasks_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    ()=> runApp(const MyApp()),
+  );
+ 
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +17,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Tasks App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => TasksBloc()
+      ..add(AddTask(task: Task(title: "Task1"),
+      )),
+      child: MaterialApp(
+        title: 'Flutter Tasks App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TasksScreen(),
       ),
-      home: TasksScreen(),
     );
   }
 }
